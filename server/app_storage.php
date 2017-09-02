dc58105a47c56c917b77e367d0c6c787519cdde0
MarcusOttor <ottor.marcus@gmail.com>
2017-09-02 20:34:47 +0300

    Update

--- server/app_storage.php
+++ server/app_storage.php
@@ -5,29 +5,33 @@
         $link = new mysqli('mysql.hostinger.com', 'u731614038_usr', '123456', 'u731614038_db');
 
         $package = $_POST['package'];
-        $limit = $_POST['limit'];
 
         $sql = "SELECT price FROM apps WHERE package = '$package'";
         $query = $link->query($sql);
         while($row = $query->fetch_assoc()){
-            $price = $row[0];
+            $price = $row['price'];
             break;
         }
 
-        $sql = "SELECT * FROM apps WHERE package != '$package' DESC LIMIT $limit";
+        if (isset($price)) {
+            $sql = "SELECT * FROM apps WHERE package <> '$package'";
 
-        $res_str = "";
+            $res_str = "";
 
-        $query = $link->query($sql);
-        while ($row = $query->fetch_assoc()) {
-            $fact_price = $price * $row['priority'] / 10;
-            $res_str .= $row['app_name'].'+|-|+'.$fact_price.'+-+'.$row['priority'].'+|-|+'.$row['image'].'||@\|/@||';
-        }
+            $query = $link->query($sql);
+            echo $query->num_rows;
+            while ($row = $query->fetch_assoc()) {
+                $fact_price = ($price * $row['priority'] / 10);
+                $res_str .= $row['app_name'] . '+|-|+' . $fact_price . '+-+' . $row['priority'] . '+|-|+' . $row['image'] . '||@\|/@||';
+            }
 
-        if (strlen($res_str) > 0) {
-            echo json_encode(substr($res_str, 0, strlen($res_str) - 9));
+            if (strlen($res_str) > 0) {
+                echo substr($res_str, 0, strlen($res_str) - 9);
+            } else {
+                echo null;
+            }
         } else {
-            echo json_encode("");
+            echo null;
         }
 
 ?>
